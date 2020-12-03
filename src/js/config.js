f980d0696b7bd66937fd8acc2d4cb01fbe5b5f36
Ikechukwu-Unegbu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2020-12-03 04:56:04 +0100

    Initial Commit Version one

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f53ef2a5-9339-4cb8-9681-a2ce0ee78167';
+export const MODAL_CLOSE_SEC = 2.5;
